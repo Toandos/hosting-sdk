3d3e3777fe6042b19631acf9caa31eca1e8b0a2c
xXjojaXx <ljonny181@gmail.com>
2026-09-18 00:32:44 +0200

    feat(ts): Add connect rpc client & server

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,2 +1,20 @@
-export * as buf from "@bufbuild/protobuf"
-export * from "./generated/models/session_pb"
+import { createClient, type ConnectRouter, type ServiceImpl, type Transport } from "@connectrpc/connect";
+
+import { SessionService } from "./generated/session_pb";
+export { type Session } from "./generated/session_pb"
+
+export class HostingClient {
+    readonly sessions;
+
+    constructor(readonly transport: Transport) {
+        this.sessions = createClient(SessionService, this.transport)
+    }
+}
+
+export abstract class HostingBackend {
+    abstract sessions: ServiceImpl<typeof SessionService>
+
+    addRoutes(router: ConnectRouter) {
+        router.service(SessionService, this.sessions)
+    }
+}
